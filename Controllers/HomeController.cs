3243d310690529e81cf93c7ff26c6aa7e1d4993b
BatyaFeldman <batyashaps1@gmail.com>
2019-02-13 22:50:49 -0500

    Initial Commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TlsScraper.Api;
+
+namespace TlsScraper.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            Scraper scraper = new Scraper();
+            IEnumerable<Article> posts = scraper.GetArticles();
+            return View(posts);
+        }
+
+       
+    }
+}
